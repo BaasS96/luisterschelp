f56ff18229744869f8dc1c3330aad5c4210d98fb
TheRealKS <koen.sauren@gmail.com>
2017-06-22 23:17:03 +0200

    Added base ocr.. Work in progress

--- scripts/ocr.js
+++ scripts/ocr.js
@@ -1,12 +1,26 @@
+var time = 0;
 function initCamera() {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         video.src = window.URL.createObjectURL(stream);
         video.play();
     });
-} else {
-    
-    
+    } else {
         alert("function not supported");
     }
+}
+function recognize() {
+    let video = document.getElementById("video");
+    console.log("recognizing...");
+    var timer = setInterval(timeri, 1);
+				OCRAD(video, {
+				}, function(text){
+                    clearInterval(timer);
+					console.log("recognized, took "+time+"ms:");
+                    time = 0;
+					console.log(text || "(empty)");
+				})
+}
+function timeri() {
+    time++;
 }

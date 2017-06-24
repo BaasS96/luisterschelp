64bf9bd224144c310a3a0f601df190d6a5c90747
TheRealKS <koen.sauren@gmail.com>
2017-06-24 20:14:12 +0200

    Made volume controls functional

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,4 +1,4 @@
-var cameraOverlayopen = false;
+var lettervolume = 1;
 
 function letterOverlay(objId) {
     var letterId = objId.id;
@@ -8,13 +8,32 @@ function letterOverlay(objId) {
     document.getElementById("letterimg").src = "images/letters/" + letter + ".png";
     document.getElementById("letteroverlay").style.marginLeft = 0;
     var letterSound = new Howl({
-      src: ['sound/letters/' + letter + '.flac', 'sound/letters/' + letter + '.mp3'],
-      volume: 1,
-      onend: function() {
-        setTimeout(function() {
-            document.getElementById("letteroverlay").style.marginLeft = "-110vw";
-        }, 750);
-      }
+        src: ['sound/letters/' + letter + '.flac', 'sound/letters/' + letter + '.mp3'],
+        volume: lettervolume,
+        onend: function() {
+            setTimeout(function() {
+                document.getElementById("letteroverlay").style.marginLeft = "-110vw";
+            }, 750);
+        }
     });
     letterSound.play();
+}
+
+function setVolume(volume, button) {
+    if (lettervolume === 1) {
+        document.getElementById("volmid").src = "images/ic_volume_down_white_48dp.png";
+    } else if (lettervolume === 0.5) {
+        document.getElementById("vollow").src = "images/ic_volume_mute_white_48dp.png";
+    } else {
+        document.getElementById("volhigh").src = "images/ic_volume_up_white_48dp.png";
+    }
+    lettervolume = volume;
+    let speaker = document.getElementById(button);
+    if (button === "vollow") {
+        speaker.src = "images/ic_volume_mute_orange_36dp.png";
+    } else if (button === "volmid") {
+        speaker.src = "images/ic_volume_down_orange_48dp.png";
+    } else {
+        speaker.src = "images/ic_volume_up_orange_48dp.png";
+    }
 }

e3292475e7c77aa9a2cd2dfd41acd33f271ad7c6
TheRealKS <koen.sauren@gmail.com>
2017-07-01 15:54:54 +0200

    tjs

--- scripts/ocr.js
+++ scripts/ocr.js
@@ -28,7 +28,7 @@ window.onload = function() {
 function initCamera() {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 
-        navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "user" } } }).then(function(stream) {
+        navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
             video.src = window.URL.createObjectURL(stream);
             video.play();
             canvas.addEventListener('click', function() {

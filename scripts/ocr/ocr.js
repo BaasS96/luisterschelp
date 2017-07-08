8880a26968a1042cb831f98c1ec4f4018cecf37f
TheRealKS <koen.sauren@gmail.com>
2017-07-08 16:16:01 +0200

    Browser detection updates

--- scripts/ocr/ocr.js
+++ scripts/ocr/ocr.js
@@ -23,7 +23,7 @@ class OCR {
     constructor(word) {
         this.word = word;
     }
-    
+
     init(onrecognized) {
         this.canvas = document.getElementById("canvas");
         this.ctx = this.canvas.getContext("2d");
@@ -32,21 +32,21 @@ class OCR {
         console.log(this.ctx);
         this.w = window.innerWidth;
         let body = document.body,
-        html = document.documentElement;
+            html = document.documentElement;
         this.h = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
         if (this.w < this.h) {
             this.canvas.setAttribute("width", this.w * 0.7);
             this.canvas.setAttribute("height", this.w * 0.7);
             this.cw = this.w * 0.7;
             this.ch = this.w * 0.7;
-        }
-        else {
+        } else {
             this.canvas.setAttribute("width", this.h * 0.7);
             this.canvas.setAttribute("height", this.h * 0.7);
             this.cw = this.h * 0.7;
             this.ch = this.h * 0.7;
         }
-        if (navigator.userAgent.includes("Gecko")) {
+        console.log(navigator.userAgent);
+        if (navigator.userAgent.includes("Firefox")) {
             if (navigator.userAgent.includes("Linux") || navigator.userAgent.includes("Android")) {
                 //We're dealing with mobile firefox here, or on linux but nobody uses that.
                 //this.canvas.style.transform = "scaleY(-1)";
@@ -58,7 +58,7 @@ class OCR {
         this.backcam = undefined;
         this.onrecognized = onrecognized;
     }
-    
+
     initCamera() {
         var t = this;
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -77,8 +77,8 @@ class OCR {
             alert("function not supported");
         }
     }
-    
-    
+
+
     tresholdimage() {
         this.photo = true;
         let img = document.getElementById("text");
@@ -87,7 +87,7 @@ class OCR {
         console.log(data);
         this.ctx.putImageData(data, 0, 0);
     }
-    
+
     recognize() {
         console.log("recognizing...");
         this.photo = true;
@@ -101,7 +101,7 @@ class OCR {
         this.onrecognized(res);
         this.photo = false;
     }
-    
+
     draw(v, x, y, w, h, c) {
         if (!this.photo) {
             ctx.drawImage(v, x, y, w, h);
@@ -109,7 +109,7 @@ class OCR {
             //ctx.drawImage(v, x, y, w, h);
         }
     }
-    
+
     //Functions to prepare the image for recognition 
     threshold(d) {
         var imageData = d.data;
@@ -135,7 +135,7 @@ class OCR {
         d.data.set(newIData);
         return d;
     }
-    
+
     getHistogram(data) {
         let histogram = Array(256);
         for (var i = 0; i < 256; i++) {
@@ -151,43 +151,43 @@ class OCR {
         }
         return histogram;
     }
-    
+
     otus(histData, total) {
-        let sum = 0;
-        for (let t = 0; t < 256; t++) sum += t * histData[t];
-        let sumB = 0;
-        let wB = 0;
-        let wF = 0;
-        let varMax = 0;
-        let threshold = 0;
-        for (let t = 0; t < 256; t++) {
-            wB += histData[t];
-            if (wB == 0) continue;
-            wF = total - wB;
-            if (wF == 0) break;
-            sumB += t * histData[t];
-            let mB = sumB / wB;
-            let mF = (sum - sumB) / wF;
-            let varBetween = wB * wF * (mB - mF) * (mB - mF);
-            if (varBetween > varMax) {
-                varMax = varBetween;
-                threshold = t;
+            let sum = 0;
+            for (let t = 0; t < 256; t++) sum += t * histData[t];
+            let sumB = 0;
+            let wB = 0;
+            let wF = 0;
+            let varMax = 0;
+            let threshold = 0;
+            for (let t = 0; t < 256; t++) {
+                wB += histData[t];
+                if (wB == 0) continue;
+                wF = total - wB;
+                if (wF == 0) break;
+                sumB += t * histData[t];
+                let mB = sumB / wB;
+                let mF = (sum - sumB) / wF;
+                let varBetween = wB * wF * (mB - mF) * (mB - mF);
+                if (varBetween > varMax) {
+                    varMax = varBetween;
+                    threshold = t;
+                }
             }
+            return threshold;
         }
-        return threshold;
-    }
-    /**
-    * @param {string} rawtext Raw input text that was recognized
-    */
+        /**
+         * @param {string} rawtext Raw input text that was recognized
+         */
     confidence(rawtext) {
-        if (rawtext.length === 0) {return false;}
+        if (rawtext.length === 0) { return false; }
         let answer = "";
         let charcount = 0;
         let letters = "",
-        i = 0,
-        abc = "abcdefghijklmnopqrstuvwxyz",
-        exceptioncharacters = "!\/\\015",
-        charsforI = "!\/\\1";
+            i = 0,
+            abc = "abcdefghijklmnopqrstuvwxyz",
+            exceptioncharacters = "!\/\\015",
+            charsforI = "!\/\\1";
         for (i = 0; i <= rawtext.length; i++) {
             let char = rawtext.charAt(i).toLowerCase();
             if (abc.indexOf(char) !== -1) {
